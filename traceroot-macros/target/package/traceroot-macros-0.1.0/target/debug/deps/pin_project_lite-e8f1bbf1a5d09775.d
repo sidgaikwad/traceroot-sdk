318d4eb72cb434c2fdc2c0f1d555f18f912a9361
sidgaikwad <gaikwadsiddharth039@gmail.com>
2025-08-15 21:06:21 +0530

    changes for cargo clean

--- traceroot-macros/target/package/traceroot-macros-0.1.0/target/debug/deps/pin_project_lite-e8f1bbf1a5d09775.d
+++
@@ -1,7 +0,0 @@
-C:\Users\gaikw\work\traceroot-sdk\traceroot-macros\target\package\traceroot-macros-0.1.0\target\debug\deps\libpin_project_lite-e8f1bbf1a5d09775.rmeta: C:\Users\gaikw\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\pin-project-lite-0.2.16\src\lib.rs
-
-C:\Users\gaikw\work\traceroot-sdk\traceroot-macros\target\package\traceroot-macros-0.1.0\target\debug\deps\libpin_project_lite-e8f1bbf1a5d09775.rlib: C:\Users\gaikw\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\pin-project-lite-0.2.16\src\lib.rs
-
-C:\Users\gaikw\work\traceroot-sdk\traceroot-macros\target\package\traceroot-macros-0.1.0\target\debug\deps\pin_project_lite-e8f1bbf1a5d09775.d: C:\Users\gaikw\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\pin-project-lite-0.2.16\src\lib.rs
-
-C:\Users\gaikw\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\pin-project-lite-0.2.16\src\lib.rs:
--- traceroot-macros/target/package/traceroot-macros-0.1.0/target/debug/deps/tracing_attributes-343c3705fbe2da50.d
+++
@@ -1,7 +0,0 @@
-C:\Users\gaikw\work\traceroot-sdk\traceroot-macros\target\package\traceroot-macros-0.1.0\target\debug\deps\tracing_attributes-343c3705fbe2da50.dll: C:\Users\gaikw\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\tracing-attributes-0.1.30\src\lib.rs C:\Users\gaikw\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\tracing-attributes-0.1.30\src\attr.rs C:\Users\gaikw\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\tracing-attributes-0.1.30\src\expand.rs
-
-C:\Users\gaikw\work\traceroot-sdk\traceroot-macros\target\package\traceroot-macros-0.1.0\target\debug\deps\tracing_attributes-343c3705fbe2da50.d: C:\Users\gaikw\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\tracing-attributes-0.1.30\src\lib.rs C:\Users\gaikw\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\tracing-attributes-0.1.30\src\attr.rs C:\Users\gaikw\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\tracing-attributes-0.1.30\src\expand.rs
-
-C:\Users\gaikw\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\tracing-attributes-0.1.30\src\lib.rs:
-C:\Users\gaikw\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\tracing-attributes-0.1.30\src\attr.rs:
-C:\Users\gaikw\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\tracing-attributes-0.1.30\src\expand.rs:
